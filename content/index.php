6e0982ca2e43a4673d948bcf62bbe532f1705f02
VS <vidya@awakentolife.org>
2026-06-20 14:47:06 +0530

    home page cta, no quotes

--- content/index.php
+++ content/index.php
@@ -5,6 +5,11 @@ echo contentBox('', 'container', true);
 
 echo returnLine('<img class="img-fluid" src="%cdn%courses/atl-workshop.jpg" />') . '</div>' . NEWLINES2;
 
+/*
 printH1InDivider('Motivational Quotes', false);
 variables(['skip_quote_heading' => BOOLYes, 'skip_quote_link' => BOOLYes]);
 runFrameworkFile('pages\quotes');
+*/
+
+printH1InDivider('VidyAntara Basics Workshop', false);
+echo getSnippet('home-va');
